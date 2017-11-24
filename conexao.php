e5392aabb42d07193a8e42b83e0a0087b11f18c1
Caliton <sr.caliton@gmail.com>
2017-11-24 20:43:09 -0200

    so estou testando

--- conexao.php
+++ conexao.php
@@ -2,15 +2,15 @@
 
     $servername = "localhost";
     $username = "root";
-    $password = "";    
-    $conn = new mysqli($servername, $username, $password);   
+    $password = "";
+    $conn = new mysqli($servername, $username, $password);
     $connbd=mysqli_select_db($conn, "truco");
     mysqli_query($conn,"SET NAMES 'utf8'");
     mysqli_query($conn,'SET character_set_connection=utf8');
     mysqli_query($conn,'SET character_set_client=utf8');
     mysqli_query($conn,'SET character_set_results=utf8');
-    
+
               
-   
-    
+
+
 ?>
